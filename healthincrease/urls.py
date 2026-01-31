11f537042cadcff7ff368340228054a3d7712c08
TKtkGg <yuki.miyamoto.0729@icloud.com>
2026-01-31 18:25:24 +0900

    軽いルーティングを施しました。

--- healthincrease/urls.py
+++ healthincrease/urls.py
@@ -1,22 +1,33 @@
-"""
-URL configuration for healthincrease project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+
+
+def simple_view(name):
+    def view(request, **kwargs):
+        parts = [name]
+        if kwargs:
+            parts.append(" ".join(f"{key}={value}" for key, value in kwargs.items()))
+        return HttpResponse(" | ".join(parts))
+
+    return view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', simple_view('top'), name='top'),
+    path('top/', simple_view('top'), name='top_redirect'),
+    path('signin/', simple_view('signin'), name='signin'),
+    path('signup/', simple_view('signup'), name='signup'),
+    path('signout/', simple_view('signout'), name='signout'),
+    path('profile/', simple_view('profile'), name='profile'),
+    path('profile/<int:user_id>/setting/', simple_view('profile_setting'), name='profile_setting'),
+    path('profile/<int:user_id>/', simple_view('profile_detail'), name='profile_detail'),
+    path('timeline/', simple_view('timeline'), name='timeline'),
+    path('timeline/<int:post_id>/', simple_view('timeline_detail'), name='timeline_detail'),
+    path('target/', simple_view('target'), name='target'),
+    path('target/<int:term_id>/', simple_view('target_detail'), name='target_detail'),
+    path('lifestyle/', simple_view('lifestyle'), name='lifestyle'),
+    path('lifestyle/<str:date>/', simple_view('lifestyle_date'), name='lifestyle_date'),
+    path('lifestyle/<str:date>/detail/', simple_view('lifestyle_detail'), name='lifestyle_detail'),
+    path('lifestyle/<str:date>/setdata/', simple_view('lifestyle_setdata'), name='lifestyle_setdata'),
 ]
