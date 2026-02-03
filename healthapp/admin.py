c2065d7bb7f73dac9ed8738a1697067c9c33da80
zdo4sa <dj369bgm135sma@gmail.com>
2026-02-03 22:06:10 +0900

    AとBの実装

--- healthapp/admin.py
+++ healthapp/admin.py
@@ -1,3 +1,13 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Category, LifeStyle
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    # リーダーの設計名(title, parent)に合わせて表示
+    list_display = ('id', 'title', 'parent')
+
+@admin.register(LifeStyle)
+class LifeStyleAdmin(admin.ModelAdmin):
+    # リーダーの設計名(date, user, category, time)に合わせて表示
+    list_display = ('date', 'user', 'category', 'time')
--- healthapp/models.py
+++ healthapp/models.py
@@ -9,6 +9,7 @@ class Category(models.Model):
         max_length=100,
         unique=True
     )
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
 
     def __str__(self):
         return self.title
