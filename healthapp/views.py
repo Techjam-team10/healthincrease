003c3f8efa52a69ad16618005b45cf6f58dbab75
youta tabuchi <tabuchiyouta0717@gmail.com>
2026-02-04 15:51:45 +0900

    プロフィール画面実装

--- healthapp/views.py
+++ healthapp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from accounts.models import User
+from .forms import UserProfileForm
 
 
 def top(request):
@@ -9,12 +11,36 @@ def home(request):
     return render(request, 'healthapp/home.html')
 
 
+@login_required
 def profile(request):
-    return render(request, "healthapp/profile.html")
+    user = request.user
+    context = {
+        'profile_user': user
+    }
+    return render(request, "healthapp/profile.html", context)
 
 
+@login_required
 def profile_setting(request, user_id):
-    return render(request, "healthapp/profile_setting.html", {"user_id": user_id})
+    user = get_object_or_404(User, id=user_id)
+    
+    # ログインユーザーが自分のプロフィール以外は編集不可
+    if request.user.id != user.id:
+        return redirect('healthapp:profile_detail', user_id=user_id)
+    
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('healthapp:profile')
+    else:
+        form = UserProfileForm(instance=user)
+    
+    context = {
+        'form': form,
+        'profile_user': user
+    }
+    return render(request, "healthapp/profile_setting.html", context)
 
 
 def profile_detail(request, user_id):
